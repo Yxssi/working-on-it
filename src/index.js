edf5aab558b751d3a95c096c5a0d02d421b66522
Yessi yessi-y-munguengui <yessi.yessimunguengui@gmail.com>
2023-03-05 00:22:28 +0100

    fix: prettier issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import './index.css';
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { sendToVercelAnalytics } from "./vitals";
-import { createRoot } from "react-dom/client";
+import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './vitals';
+import { createRoot } from 'react-dom/client';
 
-const domNode = document.getElementById("root");
+const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
