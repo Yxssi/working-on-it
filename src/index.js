b3dec7fae89a6455f7d17a29bf078573172ecc3f
Yessi yessi-y-munguengui <yessi.yessimunguengui@gmail.com>
2023-03-05 00:27:43 +0100

    fix: remove double quote in import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import App from "./App";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 import { createRoot } from 'react-dom/client';
