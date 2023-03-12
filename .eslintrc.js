c12c0ba77a0ab005584b9fffdc46eb208054795f
Yessi yessi-y-munguengui <yessi.yessimunguengui@gmail.com>
2023-03-12 23:01:52 +0100

    :construction_worker: add eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'react-hooks', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
   },
